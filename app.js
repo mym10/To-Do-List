d4036c13bada0656f86e41ba2b173915e8459080
Mythri <mythridantu1010@gmail.com>
2023-07-19 12:00:32 +0530

    Second commit, Work list added

--- app.js
+++ app.js
@@ -7,21 +7,31 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 let items = [];
+let workItems = [];
 
 app.get("/", function(req, res){
   let today = new Date();
   let options = { weekday: 'long', month: 'long', day: 'numeric' };
   let day = today.toLocaleDateString("en-US", options);
-  res.render("list", {DAY: day, NEW: items})
+  res.render("list", {ListTitle: day, NEW: items})
 });
 
 app.post("/", function(req, res){
   let item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
-})
+  if(req.body.List === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res){
+  res.render("List", {ListTitle: "Work List", NEW: workItems})
+});
 
 
 app.listen(process.env.PORT || 3000, function(req, res){
   console.log("server is up and running on port 3000");
-})
+});
